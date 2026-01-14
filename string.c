416feb70ac1c2c395ec6e2866c86185c51f799f5
l-1254-l <1254882107@qq.com>
2026-01-14 09:59:24 +0800

    string族代码第一次提交

--- string.c
+++ string.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int  main(){
+    char str1 []= "hello  world";
+    char str2 [16];
+    //计算字符串长度
+    int i=strlen(str1);
+    printf("字符串长度为%d\n",i);
 
+    //strcpy和strncpy区别
+    strcpy(str2,str1);
+    printf("%s\n",str2);
+    memset(str2, 0, 15);
+    strncpy(str2, str1, 3);
+    printf("%s\n",str2);
     printf("string族函数康复训练");
     return 0;
 
