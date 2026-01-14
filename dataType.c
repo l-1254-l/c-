f930005c0465d47add26ca74671cf590a4e10843
l-1254-l <1254882107@qq.com>
2026-01-14 14:29:19 +0800

    基本数据类型第一提交

---
+++ dataType.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int  main(){
+
+    //基本数据类型大小
+    printf("int型大小:%d\n",sizeof(int));
+
+    printf("short型大小:%d\n",sizeof(short));
+
+    printf("long型大小:%d\n",sizeof(long));
+
+    printf("float型大小:%d\n",sizeof(float));
+
+    printf("double型大小:%d\n",sizeof(double));
+
+    printf("bool型大小:%d\n",sizeof(bool));
+
+    return 0;
+}
